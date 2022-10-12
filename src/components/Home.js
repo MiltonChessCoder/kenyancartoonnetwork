ce1c61dd0b257726d2bce37230ac48438d067fde
Milton <miltonsoftwareengineer@gmail.com>
2022-10-12 13:36:31 +0300

    hooks review

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ import Recommends from "./Recommends";
 import Trending from "./Trending";
 import Viewers from "./Viewers";
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux"
 import db from "../firebase";
 import { setMovies } from "../features/movie/movieSlice";
 import { selectUserName } from "../features/user/userSlice";
